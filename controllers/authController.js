e987def116bea7978ed93ce058e5a99d21d49c43
Bienvenu <iambienvenu@gmail.com>
2024-04-27 17:12:34 +0200

    change

--- controllers/authController.js
+++ controllers/authController.js
@@ -83,8 +83,18 @@ const verifyOtp = async (req, res) => {
     console.error(error);
   }
 };
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 module.exports = {
   verifyOtp,
   verifyLogin,
+  getAllUsers,
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {
-    verifyOtp,
-    verifyLogin,
+  verifyOtp,
+  verifyLogin,
+  getAllUsers,
 } = require("../controllers/authController");
 
 router.route("/signup/verify").post(verifyOtp);
 router.route("/signin/verify").post(verifyLogin);
+router.route("/users").get(getAllUsers);
 
 module.exports = router;
